359d919adecebf0c14fb9c85a3a684d39a80165c
fredi <61924008+fredi286@users.noreply.github.com>
2020-06-11 14:29:44 +0700

    Add files via upload

---
+++ omnivora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction.AbstractClass
+{
+    public class Omnivora : Hewan
+    {
+        public override void Jenis()
+        {
+            Console.WriteLine("Hewan pemakan tumbuh tumbuhan dan daging.");
+            Console.WriteLine("Hewan yang sangat buas.");
+        }
+    }
+}
